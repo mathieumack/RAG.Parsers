6bb6c94b6bdab208136ede79ac4e33bceabe55f0
Mathieu Mack <mathieu.mack@viseo.com>
2023-12-19 09:56:58 +0100

    Init solution

---
+++ src/My.Converters.ToMardown.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace My.Converters.ToMardown.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
