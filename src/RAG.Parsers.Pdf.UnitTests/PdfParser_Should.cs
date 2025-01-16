209bd2d80f8da39fb9b18202a58658f24f1de47a
Mathieu Mack <mathieu.mack@viseo.com>
2025-01-16 23:37:21 +0100

    Add more unit tests

--- src/RAG.Parsers.Pdf.UnitTests/PdfParser_Should.cs
+++ src/RAG.Parsers.Pdf.UnitTests/PdfParser_Should.cs
@@ -6,7 +6,7 @@ namespace RAG.Parsers.Pdf.UnitTests;
 public class PdfParser_Should
 {
     [TestMethod]
-    //[DataRow("TestFiles/TestSample.pdf", DisplayName = "Sample document")]
+    [DataRow("TestFiles/TestSample.pdf", DisplayName = "Sample document")]
     [DataRow("TestFiles/TestSample2Columns.pdf", DisplayName = "Sample document on 2 columns")]
     public void ConvertWordToMarkdown_WhenOK(string documentPath)
     {
