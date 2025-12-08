02e1bf875becf115fe647d5b1e385484cb59858e
Cochinho Louis <louis.cochinho@viseo.com>
2025-12-08 10:35:57 +0100

    test name rebranding

--- src/RAG.Parsers.Pdf.UnitTests/PdfParser_Should.cs
+++ src/RAG.Parsers.Pdf.UnitTests/PdfParser_Should.cs
@@ -9,7 +9,7 @@ public class PdfParser_Should
     [TestMethod]
     [DataRow("TestFiles/TestSample.pdf", DisplayName = "Sample document")]
     [DataRow("TestFiles/TestSample2Columns.pdf", DisplayName = "Sample document on 2 columns")]
-    public void ConvertWordToMarkdown_WhenOK(string documentPath)
+    public void ConvertPdfToMarkdown_WhenOK(string documentPath)
     {
         // Arrange
         using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
